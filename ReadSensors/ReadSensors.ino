bb3990357d07dec13459da49b195a2202e7d22e6
Kylie Outlaw <outlawkylie@gmail.com>
2021-05-01 18:58:44 -0500

    Add interrupt and ISR, check in main loop for reading sensors

--- ReadSensors/ReadSensors.ino
+++ ReadSensors/ReadSensors.ino
@@ -1,136 +1,95 @@
 #include <OneWire.h>
+#include <dht.h>
 
 #define TdsSensorPin A0
 #define TempSensorPin 3
+#define HumSensorPin 12
 
-#define VREF 5.0
-#define SCOUNT 30
-int TDSbuff[SCOUNT];
-int TEMPbuff[SCOUNT];
-int bufferTemp[SCOUNT];
-int buffIdx = 0;
+#define VREF 5.0 //reference voltage for TDS
+#define SCOUNT 30 //sample count
+int TDSbuff[SCOUNT]; //buffer to hold samples of TDS
+int TEMPbuff[SCOUNT]; //buffer to hold samples of TEMPERATURE
+float HUMbuff[SCOUNT]; //buffer to hold samples of HUMIDITY
+int bufferTemp[SCOUNT]; //temp buffer
+static int buffIdx = 0; 
 int cpyIdx = 0;
-float avgVolt = 0;
-float tdsValue = 0;
-float temperature = 25;
+float avgVolt = 0; //value for average voltage
+float tdsValue = 0; //value for tds
+float temperature = 25; //reference temperature
+static bool flag = false;
+
+const uint16_t t1_load = 0;
+const uint16_t t1_comp = 31250;
 
 OneWire ds(TempSensorPin);
+dht DHT;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(TdsSensorPin, INPUT);
   pinMode(TempSensorPin, INPUT);
+  pinMode(HumSensorPin, INPUT);
+
+  //Reset timer1 control register A
+  TCCR1A = 0;
+
+  //Enable CTC mode
+  TCCR1B &= ~(1<<WGM13);
+  TCCR1B |= (1<<WGM12);
+
+  //Set prescalar to 1024
+  TCCR1B |= (1<<CS12);
+  TCCR1B |= (1<<CS10);
+  TCCR1B &= ~(1<<CS11);
+
+  //Set up timer compare and load values
+  TCNT1 = t1_load;
+  OCR1A = t1_comp;
+
+  // Enable compare interrupt
+  TIMSK1 = (1<<OCIE1A);
+
+  //Enable global interrupts
+  sei();  
 }
 
 void loop() {  
-  // put your main code here, to run repeatedly:
-  static unsigned long analogSampleTime = millis();
-  if( millis()-analogSampleTime > 100U)
+delay(500);
+  if(flag == true)
   {
-    analogSampleTime = millis();
+    int DHTSample = DHT.read11(HumSensorPin);
+    HUMbuff[buffIdx] = DHT.humidity;
     TDSbuff[buffIdx] = analogRead(TdsSensorPin);
     TEMPbuff[buffIdx] = getTemp();
-    buffIdx++;
+    buffIdx++; //increase buffer indexes
+    
+    // Print the data every 60 seconds
     if(buffIdx == SCOUNT)
     {
-      buffIdx = 0;
-    }
-  }
-   static unsigned long printTimepoint = millis();
-   if(millis()-printTimepoint > 1000U)
-   {
-      printTimepoint = millis();
-      for(cpyIdx=0;cpyIdx<SCOUNT;cpyIdx++)
-        bufferTemp[cpyIdx]= TDSbuff[cpyIdx];
-      avgVolt = getMedianNum(bufferTemp,SCOUNT) * (float)VREF / 1024.0; // read the analog value more stable by the median filtering algorithm, and convert to voltage value
-      float compensationCoefficient=1.0+0.02*(temperature-25.0);    //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0));
-      float compensationVolatge=avgVolt/compensationCoefficient;  //temperature compensation
-      tdsValue=(133.42*compensationVolatge*compensationVolatge*compensationVolatge - 255.86*compensationVolatge*compensationVolatge + 857.39*compensationVolatge)*0.5; //convert voltage value to tds value
-      //Serial.print("voltage:");
-      //Serial.print(avgVolt,2);
-      //Serial.print("V   ");
+      // Print TDS Avg
+      float tds = getAvgTDS(TDSbuff, SCOUNT);
       Serial.print("TDS Value:");
       Serial.print(tdsValue,0);
       Serial.println("ppm");
-
-      // Temperature Avg
-      float temperature = getTemp();
+      
+      // Print Temperature Avg
+      float temperature = getAvgTemp(TEMPbuff, SCOUNT);
       Serial.print("Temp Value:");
       Serial.print(temperature*1.8 +32);
       Serial.print("°F\n");
-      Serial.print("* * * * * * * * * *\n");
-   }
+  
+      //Print Humidity Avg
+      float humidityAvg = getAvgHum(HUMbuff, SCOUNT);
+      Serial.print("Humidity Value:");
+      Serial.print(humidityAvg);
+      Serial.print("\n* * * * * * * * * *\n");
+      buffIdx = 0;
+    }
+    flag = false;
+  }
 }
 
-//For TDS
-int getMedianNum(int bArray[], int iFilterLen) 
+ISR(TIMER1_COMPA_vect)
 {
-      int bTab[iFilterLen];
-      for (byte i = 0; i<iFilterLen; i++)
-      bTab[i] = bArray[i];
-      int i, j, bTemp;
-      for (j = 0; j < iFilterLen - 1; j++) 
-      {
-      for (i = 0; i < iFilterLen - j - 1; i++) 
-          {
-        if (bTab[i] > bTab[i + 1]) 
-            {
-        bTemp = bTab[i];
-            bTab[i] = bTab[i + 1];
-        bTab[i + 1] = bTemp;
-         }
-      }
-      }
-      if ((iFilterLen & 1) > 0)
-    bTemp = bTab[(iFilterLen - 1) / 2];
-      else
-    bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
-      return bTemp;
-}
-
-float getTemp(){
-  //returns the temperature from one DS18S20 in DEG Celsius
-
-  byte data[12];
-  byte addr[8];
-
-  if ( !ds.search(addr)) {
-      //no more sensors on chain, reset search
-      ds.reset_search();
-      return -1000;
-  }
-
-  if ( OneWire::crc8( addr, 7) != addr[7]) {
-      Serial.println("CRC is not valid!");
-      return -1000;
-  }
-
-  if ( addr[0] != 0x10 && addr[0] != 0x28) {
-      Serial.print("Device is not recognized");
-      return -1000;
-  }
-
-  ds.reset();
-  ds.select(addr);
-  ds.write(0x44,1); // start conversion, with parasite power on at the end
-
-  byte present = ds.reset();
-  ds.select(addr);
-  ds.write(0xBE); // Read Scratchpad
-
-
-  for (int i = 0; i < 9; i++) { // we need 9 bytes
-    data[i] = ds.read();
-  }
-
-  ds.reset_search();
-
-  byte MSB = data[1];
-  byte LSB = data[0];
-
-  float tempRead = ((MSB << 8) | LSB); //using two's compliment
-  float TemperatureSum = tempRead / 16;
-
-  return TemperatureSum;
+flag = true;
 }
